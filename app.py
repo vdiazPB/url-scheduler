05449a3f29eba6f7cc8460eb6f192867f0c7c169
Victor Diaz <vdiaz@weareamazing.com>
2025-06-21 13:00:36 -0700

    Fix port binding for Render

--- app.py
+++ app.py
@@ -163,4 +163,6 @@ function setTime() {
 '''
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
